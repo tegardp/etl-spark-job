acfd318a91cd70deb729751f521ee472780496c7
tegardp <tegar1195@gmail.com>
2021-06-09 17:36:12 +0700

    create bash-script and spark_job

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# check
+# ETL Using PySpark on Google Dataproc
+
+**How can we process a huge amount of data automatically without writing a script repeatedly?**
+
+In this project, you have a huge amount of data from your client on your local computer stored in `data` folder. You should transform the data and store them into BigQuery as your Data Warehouse
+
+## Project Overview
+
+1. Move data from local storage to BigQuery
+
+## Tech Stack
+
+We're going to use some services from Google Cloud Platform here:
+
+1. Google Cloud SDK
+2. Google Dataproc
+3. PySpark
+
+## Data
+
+Flight data, you can see the datasets on `data` folder
+
+## Requirements
+
+1. Google Cloud SDK. To set it up read it [here](https://cloud.google.com/sdk/docs/quickstart#installing_the_latest_version)
+2. Git. For windows you can download git bash from [here](https://git-scm.com/downloads)
+3. PySpark Knowledge. Read it [here](https://jacobcelestine.com/knowledge_repo/colab_and_pyspark/)
+
+## Installation
+
+1. run command `$ chmod +x bash_script.sh`
+2. then run `$ ./bash_script.h`
---
+++ spark_job.py
@@ -0,0 +1,41 @@
+import os
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StringType, IntegerType, StructType, StructField
+from google.cloud import storage
+
+
+# Create Spark Session
+spark = SparkSession \
+            .builder \
+            .appName("FlightData") \
+            .getOrCreate()
+
+# Define Schema
+fields = [
+            StructField("airline_code", StringType(), True),
+            StructField("airtime", IntegerType(), True),
+            StructField("arrival_delay", IntegerType(), True),
+            StructField("arrival_time", StringType(), True),
+            StructField("destination_airport", StringType(), True),
+            StructField("distance", IntegerType(), True),
+            StructField("flight_date", StringType(), True),
+            StructField("flight_num", IntegerType(), True),
+            StructField("id", IntegerType(), True),
+            StructField("source_airport", StringType(), True),
+         ]
+schema = StructType(fields)
+
+# Create Empty DataFrame with defined schema
+flightData = spark.createDataFrame([], schema)
+
+# Fill DataFrame with Flight Data
+files = ["gs://academi/2021-04-27.json","gs://academi/2021-04-28.json","gs://academi/2021-04-29.json", "gs://academi/2021-04-30.json"]
+for file in files:
+    flightData = spark.read.json(file, schema=schema).union(flightData)
+
+flightData.withColumn('flight_date', F.date_add(flightData['flight_date'], 723)).show()
+
+# Saving the data to BigQuery
+flightData.write.format('bigquery') \
+  .option('table', 'dwh.flight_data') \
+  .save()
